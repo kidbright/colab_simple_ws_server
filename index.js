f594a60297642b4b3a92574d05c3d1d4228f89e0
kidbright <kidbright2000@gmail.com>
2021-09-15 18:47:20 +0700

    update index.js file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const WebSocket = require('ws');
+const ws_port = 3232;
+
+log('websocket server listening on port ' + port);
 const ws_server = new WebSocket.Server({
-	port: 3232
+	port: ws_port
 });
 
 ws_server.on('connection', function(socket) {
@@ -14,3 +17,7 @@ ws_server.on('connection', function(socket) {
 		// });
 	});
 });
+
+function log(str, tag) {
+	console.log(moment().format('YYYY-MM-DD HH:mm:ss.SSS') + '\t' + tag + '\t' + str);
+}
